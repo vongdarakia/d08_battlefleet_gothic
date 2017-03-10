50deb6290abb9f247b3c04e90573c2087c63e44d
Akia Vongdara <avongdar@e1z3r8p22.42.us.org>
2017-03-09 21:27:33 -0800

    changed name. added direction variables in game

---
+++ classes/Battlefleet/Battlefleet.class.php
@@ -0,0 +1,21 @@
+<?php 
+require_once('Weapon.class.php');
+
+class Battlefleet {
+	public static $N = 0;
+	public static $E = 1;
+	public static $S = 2;
+	public static $W = 3;
+
+	private $_currentPhase;
+	private $_playerTurn;
+	private $_gameSize;
+
+	public function __construct() {
+
+	}
+
+
+}
+
+?>
