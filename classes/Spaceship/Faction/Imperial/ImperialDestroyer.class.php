73733e4f87245aef11b84d93bbfa10bc5fe438ca
James Shi <jshi@student.42.us.org>
2017-03-09 23:52:53 -0800

    Added specific spaceships

---
+++ classes/Spaceship/Faction/Imperial/ImperialDestroyer.class.php
@@ -0,0 +1,13 @@
+<?php
+require_once './Imperial.class.php';
+
+class ImperialFrigate extends Imperial {
+	public static function doc() {
+		return file_get_contents('./ImperialFrigate.doc.txt');
+	}
+
+	public function __construct( $x, $y ) {
+		parent::__construct(array('length' => 1, 'width' => 3, 'hp' => 4, 'pp' => 10, 'speed' => 18, 'handle' => 3, 'x' => $x, 'y' => $y));
+	}
+}
+?>
