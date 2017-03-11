5ad30f3c9834f45b1ce1988fbee4900abaaf2181
Mikhail Filipchuk <mfilipch@e1z3r8p20.42.us.org>
2017-03-10 17:46:44 -0800

    movement functions

--- classes/Game/movement.php
+++ classes/Game/movement.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'Spaceship.class.php'
 
+
+// Orientation/Direction: 0 - N, 1 - E, 2 - S, 3 - W
+
 public function check_validity ( $map, $ship, $x, $y, $orient ) {
 
 	//Setup env
@@ -15,58 +18,75 @@ public function check_validity ( $map, $ship, $x, $y, $orient ) {
 	$ymax = $max->ymax;
 
 	// Checking interval;
-	if ($orient == 0) {
-		$intx = $x - $length;
-		$inty = $y - $width;
-	}
-	else if ($orient == 1) {
-		$intx = $x + $width;
-		$inty = $y - $length;
-	}
-	else if ($orient == 2) {
-		$intx = $x + $length;
-		$inty = $y + $width;
-	}
-	else if ($orient == 3) {
-		$intx = $x - $width;
-		$inty = $y + $length;
-	}
-	
-	if ($orient != $direction) {
-		$px1 = min($x, $intx);
-		$py1 = min($y, $inty);
-		$px2 = max($x, $intx);
-		$py2 = max($y, $inty);
-	}
-	else {
-		$px1 = min($x, $intx, $x0);
-		$py1 = min($y, $inty, $y0);
-		$px2 = max($x, $intx, $x0);
-		$py2 = max($y, $inty, $y0);
-	}
+	$sw = $orient % 2;
+	$intx = $x + $length * (1 - $sw) + $width  * $sw;
+	$inty = $y + $length  * $sw + $width  * (1 - $sw);
 
-	// Check for obstacles
+	// Check for obstacles collisions
 	foreach ($obstacle as $row => $cols) {
 		foreach ($cols as $col => $val) {
-			if ($col >= $px1 && $col <= $px2 && $row >= $py1 && $row <= $py2) {
+			if ($col >= $x0 && $col <= $intx && $row >= $y0 && $row <= $inty) {
 				return -1;
 			}
 		}
 	}
 
-	// Check map border
-	if ($px2 > $xmax || $py2 > $ymax || $px1 < 0 || $py1 < 0)
+	// Check map border collisions
+	if ($intx > $xmax || $inty > $ymax || $x < 0 || $y < 0)
 		return -1;
 
-	// Check for ships
+	// Check for ships collisions
 	foreach ($ships as $key => $value) {
-		$corrds = get_coords($ships[$key]);
-		if (($coords[1][0] >= $px1 && $coords[1][0] <= $px2 && $coords[0][0] >= $py1 && $coords[0][0] <= $py2) ||($coords[1][0] >= $px1 && $coords[1][0] <= $px2 && $coords[0][1] >= $py1 && $coords[0][1] <= $py2) ||
-			($coords[1][1] >= $px1 && $coords[1][1] <= $px2 && $coords[0][0] >= $py1 && $coords[0][0] <= $py2) ||
-			($coords[1][1] >= $px1 && $coords[1][1] <= $px2 && $coords[0][1] >= $py1 && $coords[0][1] <= $py2)) {
-			
-			damage_ship( $ships[$key] )	
-			return 0;
+		
+		//env
+		$shp_orient = $ships[$key]->direction;
+		$shp_length = $ships[$key]->length;
+		$shp_width = $ships[$key]->width;
+		$shp_x = $ships[$key]->x;
+		$shp_y = $ships[$key]->y;
+
+		$shp_sw = $shp_orient % 2;
+		$shp_intx = $shp_x + $shp_length * (1 - $shp_sw) + $shp_width  * $shp_sw;
+		$shp_inty = $shp_y + $shp_length  * $shp_sw + $shp_width  * (1 - $shp_sw);
+
+		if ($orient == $direction) {
+			// Collisions during sraight flying
+			if (($shp_x >= $x0 && $shp_x <= $intx && $shp_y >= $y0 && $shp_x <= $inty) ||
+				($shp_x >= $x0 && $shp_x <= $intx && $shp_inty >= $y0 && $shp_inty <= $inty) ||
+				($shp_intx >= $x0 && $shp_intx <= $intx && $shp_y >= $y0 && $shp_y <= $inty) ||
+				($shp_intx >= $x0 && $shp_intx <= $intx && $shp_inty >= $y0 && $shp_inty <= $inty)) {
+				
+				// That ship should take damage too if our speed is greater than normal
+				//  damage_ship( $ships[$key] );
+				// 	damage_ship( $ship );
+				
+				// Ships become stationary
+				// attr->stationary = True;
+
+				// Moved ship stop on collisin, change ship attributes
+				if ($orient == 0)
+					$ship->y = $shp_inty;
+				else if ($orient == 2)
+					$ship->y = $shp_y - $ship->width;
+				else if ($orient == 1)
+					$ship->x = $shp_x - $ship->width;
+				else if ($orient = 3)
+					$ship->x = $shp_intx;
+				return 0;
+			}
+		}
+		else {
+			// Collisions during turning
+			if (($shp_x >= $x && $shp_x <= $intx && $shp_y >= $y && $shp_x <= $inty) ||
+				($shp_x >= $x && $shp_x <= $intx && $shp_inty >= $y && $shp_inty <= $inty) ||
+				($shp_intx >= $x && $shp_intx <= $intx && $shp_y >= $y && $shp_y <= $inty) ||
+				($shp_intx >= $x && $shp_intx <= $intx && $shp_inty >= $y && $shp_inty <= $inty)) {
+				
+				// That ship should take damage too if our speed is greater than normal
+				// Damaged ship should become stationary
+				damage_ship( $ships[$key] )	
+				return 0;
+			}
 		}
 	}
 	
@@ -114,10 +134,6 @@ public function move_ship( $map, $ship, $mv_num, $verbose ) {
 		// Ship destroyed
 		return -1;
 	}
-	else {
-		// Ship takes damage
-		return 0;
-	}
 }
 
 public function turn_ship( $map, $ship, $dir, $verbose ) {
@@ -134,28 +150,20 @@ public function turn_ship( $map, $ship, $dir, $verbose ) {
 			, $x, $y, $orient);
 	}
 
-	// Initial size set for rotation with correction
-	if ((($length + 1) % 2) ^ (($width + 1) % 2)) {
-		$length -= ($length + 1) % 2;
-		$width -= ($width + 1) % 2;
-	}
+	// Correction should be set and implemented in rotation section below
+	// if ((($length + 1) % 2) ^ (($width + 1) % 2)) {
+	// 	$length -= ($length + 1) % 2;
+	// 	$width -= ($width + 1) % 2;
+	// }
 	
 	// Rotate and change coordinates
-	if (($orient == 0 && $dir == 1) || ($orient == 3 && $dir == -1)) {
-		$x -= intdiv($width + $length, 2);
-		$y -= intdiv($width - $length, 2);
-	}
-	else if (($orient == 1 && $dir == 1) || ($orient == 0 && $dir == -1)) {
-		$y -= intdiv($width + $length, 2);
-		$x += intdiv($width - $length, 2);
-	}
-	else if (($orient == 2 && $dir == 1) || ($orient == 1 && $dir == -1)) {
-		$x += intdiv($width + $length, 2);
-		$y += intdiv($width - $length, 2);
-	}
-	else if (($orient == 3 && $dir == 1) || ($orient == 2 && $dir == -1)) {
-		$y += intdiv($width + $length, 2);
+	if ($orient == 0 || $orient == 2) {
 		$x -= intdiv($width - $length, 2);
+		$y += intdiv($length - $width, 2);
+	}
+	else if ($orient == 1 || $orient == 3) {
+		$x += intdiv($length - $width, 2);
+		$y -= intdiv($length - $width, 2);
 	}
 
 	// Change ship direction
@@ -183,10 +191,6 @@ public function turn_ship( $map, $ship, $dir, $verbose ) {
 	else if ($valid == -1) {
 		// Ship destroyed
 		return -1;
-	}
-	else {
-		// Ship takes damage
-		return 0;
 	}
 }
 
