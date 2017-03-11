4e0bc344ded4144cf085edb49791f7878feda8c6
Akia Vongdara <avongdar@e1z3r8p22.42.us.org>
2017-03-10 22:39:52 -0800

    get game state fixed by added required files

--- actions/get_game_state.php
+++ actions/get_game_state.php
@@ -3,20 +3,28 @@
 	ini_set('display_errors', 1);
 	date_default_timezone_set('America/Los_Angeles');
 	require_once __DIR__ . '/../classes/Battlefleet/Battlefleet.class.php';
+	require_once __DIR__ . '/../classes/Battlefleet/Object.class.php';
+	require_once __DIR__ . '/../classes/Battlefleet/Player.class.php';
+	require_once __DIR__ . '/../classes/Spaceship/Spaceship.class.php';
 	require_once __DIR__ . '/../classes/Spaceship/Faction/Imperial/ImperialFrigate.class.php';
+	// require_once __DIR__ . '/../classes/Spaceship/Faction/Imperial/ImperialFrigate.class.php';
 	require_once __DIR__ . '/../classes/Weapon/NauticalLance.class.php';
-	
+	require_once __DIR__ . '/../classes/Weapon/Weapon.class.php';
 	// $bf = new Battlefleet();
 	// $ship = new ImperialFrigate(0, 0);
 	// $ship2 = new ImperialFrigate(0, 0);
 	// $bf->getCurrentPlayer()->addShip($ship);
 	// $bf->getCurrentPlayer()->addShip($ship2);
 
+	require_once 'get_game.php';
+	// saveGame($bf, $gameFile);
 
-	require_once 'game_save.php';
+	$game = loadGame($gameFile);
 
-	$bf =loadGame($gameFile);
+	// require_once 'get_game.php';
 
-	echo json_encode($bf);
+	// $bf =loadGame($gameFile);
+
+	echo json_encode($game);
 
 ?>
