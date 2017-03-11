c438f94d2030709966139f4cf7a90cb02a3bf927
Akia Vongdara <avongdar@e1z3r8p22.42.us.org>
2017-03-10 22:17:30 -0800

    actions

---
+++ actions/move_ship.php
@@ -0,0 +1,7 @@
+<?php 
+	require_once 'get_game.php';
+
+	$_POST['distance'];
+	$_POST['rotation']; // 0 nothing, -1 counter, 1 clockwise 90
+	$_POST['ship_id'];
+?>
---
+++ actions/spend_pp.php
@@ -0,0 +1,12 @@
+<?php 
+	require_once 'get_game.php';
+
+	// $numShips = intval($_POST['num_ships']);
+	$_POST['ship_id']
+	$_POST['speed']
+	$_POST['shield']
+	$_POST['weapon']
+	$_POST['repair']
+
+	// saveGame($game, $gameFile);
+?>
