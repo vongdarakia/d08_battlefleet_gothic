7dde15a61997e252d8497bb45f883d6b97944e3a
Akia Vongdara <avongdar@e1z3r8p22.42.us.org>
2017-03-12 18:53:30 -0700

    removed echo ship

--- actions/p1_move_ship.php
+++ actions/p1_move_ship.php
@@ -7,7 +7,7 @@ playerCheck($game, $playerID);
 $dist = isset($_POST['distance']) ? intval($_POST['distance']) : 0; 
 $shipID = isset($_POST['ship_id']) ? intval($_POST['ship_id']) : 0; 
 $ship = $game->getShipByID($shipID);
-echo $ship;
+
 if ($ship) {
 	if ($ship->moveShip($dist, $game->getMap())) {
 		$game->updateMap();
