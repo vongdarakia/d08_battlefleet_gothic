07523d2a69b854305583ed2ef1e47d31f8faa1a6
Akia Vongdara <avongdar@e1z3r8p22.42.us.org>
2017-03-12 17:28:55 -0700

    rotations done

---
+++ actions/p1_rotate_ship.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'get_game.php';
+$rot = intval($_POST['rotation']);		// 0 nothing (default), -1 counter, 1 clockwise 90
+$shipID = intval($_POST['ship_id']);
+$ship = $game->getShipByID($shipID);
+
+if ($ship) {
+	if ($ship->turnShip($rot)) {
+		$game->updateMap();
+		saveGame($game, $gameFile);
+		header("Content-Type: application/json");
+		echo json_encode($game);
+		exit();
+	}
+}
+sendError();
+?>
